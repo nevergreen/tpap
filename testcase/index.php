537c58b7d51807013744aa9355ff9bf6187eb6fd
shiba <shiba@taobao.com>
2013-09-24 09:14:15 +0800

    update testcase

--- testcase/index.php
+++ testcase/index.php
@@ -9,7 +9,10 @@
 <ul>
     <li><a href="./1.3.0/baseapi.php">语言基本api和kissy基础模块</a></li>
     <li><a href="./1.3.0/calendar.php">日历</a></li>
+    <li><a href="./1.3.0/resizable.php">resizeable</a></li>
+    <li><a href="./1.3.0/imagezoom.php">放大镜imagezoom</a></li>
     <li><a href="./gallery/kcharts1.1.php">kcharts</a></li>
+    <li><a href="./gallery/layeranim1.1.php">layeranim</a></li>
     <li><a href="./thirdparty/alimap.php">alimap</a></li>
     <li><a href="./thirdparty/jssdk.php">jssdk</a></li>
 </ul>
