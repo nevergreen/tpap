de7ee1c51761437a76b01605725dbace721a0969
shiba <shiba@taobao.com>
2013-10-09 11:52:52 +0800

    update css testcase

--- testcase/1.3.0/dom-css.js
+++ testcase/1.3.0/dom-css.js
@@ -24,6 +24,70 @@ describe('css test', function () {
         }
     }
 
+    describe('support css2', function(){
+       it('support simple css2(color height position font-family font-size z-index left', function(){
+           var div = document.createElement('div');
+           document.body.appendChild(div);
+
+           div.style.height = '100px';
+           expect(div.style.height).toBe('100px');
+
+           div.style.color = 'red';
+           expect(div.style.color).toBe('red');
+
+           div.style.position = 'relative';
+           expect(div.style.position).toBe('relative');
+
+           div.style.position = 'fixed';
+           expect(div.style.position).toBe('fixed');
+
+           div.style.zIndex = "100";
+           expect(div.style.zIndex).toBe('100');
+
+           div.style.zIndex = "1000000000";
+           expect(div.style.zIndex).toBe('1000000000');
+
+           div.style.border = "1px solid red";
+           expect(div.style.border).toBe('1px solid red');
+
+           div.style.top = "10px";
+           expect(div.style.top).toBe('10px');
+
+           div.style.left = "10px";
+           expect(div.style.left).toBe('10px');
+
+           div.style.fontSize = "10px";
+           expect(div.style.fontSize).toBe('10px');
+
+           div.style.fontFamily = '"selfdefined1"';
+           expect(div.style.fontFamily).toBe('selfdefined1');
+
+           div.style.fontFamily = 'serif';
+           expect(div.style.fontFamily).toBe('serif');
+
+           div.style.visibility = 'hidden';
+           expect(div.style.visibility).toBe('hidden');
+
+           div.style.overflow = 'scroll';
+           expect(div.style.overflow).toBe('scroll');
+
+
+       });
+
+       it('support filter in ie', function(){
+           if(KISSY.UA.ie ){
+               var div = document.createElement('div');
+               document.body.appendChild(div);
+               div.innerHTML = 'opacity';
+
+               div.style.filter = 'alpha(opacity=30)';
+               expect(div.style.filter).toBe('alpha(opacity=30)');
+
+           }
+       })
+
+    });
+
     describe('support css3', function () {
         it('support border-radius', function () {
             var div = document.createElement('div');
@@ -35,12 +99,30 @@ describe('css test', function () {
             expect(div.style.borderRadius.indexOf('100px')).not.toBe(-1);
         });
 
+        it('support box-sizing', function(){
+            var div = document.createElement('div');
+            document.body.appendChild(div);
+
+            div.style.boxSizing = 'border-box';
+            expect(div.style.boxSizing).toBe('border-box');
+        })
+
+
+        it('support box-shadow', function(){
+            var div = document.createElement('div');
+            document.body.appendChild(div);
+
+            div.style.boxShadow = 'red 1px 1px 1px inset';
+            expect(div.style.boxShadow).toBe('red 1px 1px 1px inset');
+        })
+
         it('support transition', function () {
             var div = document.createElement('div');
             document.body.appendChild(div);
             div.style.height = '100px';
             div.style.width = '100px';
             div.style.border = '1px solid red';
+
             waits(200);
             runs(function () {
                 css(div, 'transition-property', 'width,height');
